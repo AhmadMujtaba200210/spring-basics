80d2eb99c1e58da92003203e229059675d4e4ffc
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-03-02 10:03:29 +0500

    pract: practicing POJO

--- JavaBeanVsSpringBean/src/main/java/nb/learn/javabeanvsspringbean/MainClass.java
+++ JavaBeanVsSpringBean/src/main/java/nb/learn/javabeanvsspringbean/MainClass.java
@@ -1,5 +1,7 @@
 package nb.learn.javabeanvsspringbean;
 
+import java.io.Serializable;
+
 class Pojo{
     private String text;
     private int number;
@@ -12,6 +14,34 @@ class Pojo{
                 '}';
     }
 }
-public class MainClass {
+class JavaBean implements Serializable { //EJB: Enterprise Java Bean
+    // 1: EJB will have no argument constructor
+    private String text;
+    private int number;
+    public JavaBean() {
+    }
+    //2: public getters and setters
+    public String getText() {
+        return text;
+    }
 
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    // EJB class is serialization
+}
+public class MainClass {
+    public static void main(String[] args) {
+        Pojo pojo= new Pojo(); // POJO: Plain Old Java Object. Every java object is Pojo
+        System.out.println(pojo);
+    }
 }
