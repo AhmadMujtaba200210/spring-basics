56aba05ea55efadd2a0cbf9fa211240900ec7932
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-03-06 20:37:55 +0500

    build: created beans with field injector

--- basics 02/src/main/java/examples/MySQLDataService.java
+++ basics 02/src/main/java/examples/MySQLDataService.java
@@ -1,2 +1,15 @@
-package examples;public class MySQLDataService {
+package examples;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+
+@Component
+@Qualifier("MySQL")
+public class MySQLDataService implements DataService{
+    @Override
+    public int[] retrieveData() {
+        return new int[]{4,5,2,6,1,0,9,3};
+    }
 }
